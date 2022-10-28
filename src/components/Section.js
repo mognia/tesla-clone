fe122bf6097ea06a479b3d8736a3a6f6cf540008
Mognia <1mognia@gmail.com>
2022-10-28 14:22:59 +0330

    adding section component and all required images

---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function Section() {
+    return (
+        <Warp>
+
+        </Warp>
+    )
+}
+
+export default Section
+const Warp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+`
