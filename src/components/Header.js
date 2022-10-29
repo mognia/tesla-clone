0e56bd75b02a857a9662d91daf2233841151e76b
Mognia <1mognia@gmail.com>
2022-10-29 19:40:19 +0330

    adding side nav and its functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,42 @@
 import styled from "styled-components";
-import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import {useState} from "react";
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false)
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg"/>
             </a>
-        <Menu>
-            <a href="#">Model S</a>
-            <a href="#">Model 3</a>
-            <a href="#">Model X</a>
-            <a href="#">Model Y</a>
-        </Menu>
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </Menu>
             <RightMenu>
                 <a href="#">Support</a>
                 <a href="#">Shop</a>
                 <a href="#">Account</a>
-                <a href="#">Menu</a>
+                <a onClick={() => setBurgerStatus(true)}>Menu</a>
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+                <li><a href="#">Lorem Iprisum</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -31,10 +49,10 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 0 20px;
-  top:0;
+  top: 0;
   left: 0;
   right: 0;
-
+  z-index: 1;
 
 `;
 const Menu = styled.div`
@@ -42,23 +60,55 @@ const Menu = styled.div`
   align-items: center;
   justify-content: center;
   flex: 1;
-  a{
+
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
   }
-  @media(max-width: 768px) {
+
+  @media (max-width: 768px) {
     display: none;
   }
 `;
 const RightMenu = styled.div`
-    a{
-      font-weight: 600;
-      text-transform: uppercase;
-      margin-right: 10px;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
 
-    }
+  }
 `
-const CustomMenu = styled(MenuIcon) `
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  list-style: none;
+  z-index: 16;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.2s;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid;
+
+    a {
+      font-weight: 600;
+    }
+  }
+`
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
